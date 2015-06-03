3c75d814a1b0d2af2a7689baa2fdf7f82350a300
dmitry.halkin <d.halkin@levi9.com>
2015-06-04 00:52:53 +0300

    fixtures

---
+++ src/Assignment/JamStorageBundle/DataFixtures/ORM/JamArchiveLoader.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Assignment\JamStorageBundle\DataFixtures\ORM;
+
+use Hautelook\AliceBundle\Alice\DataFixtureLoader;
+use Nelmio\Alice\Fixtures;
+
+class JamStorageLoader extends DataFixtureLoader
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getFixtures()
+    {
+        return  array(
+            __DIR__ . '/jam.yml',
+
+        );
+    }
+
+    public function jamName()
+    {
+        $names = array(
+            'Apricot', 'Raspberries', 'Cherry-plum'
+        );
+
+        return $names[array_rand($names)];
+    }
+
+}
